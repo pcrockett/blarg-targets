bc0b93ac4eec476ed4fadd20a00f49422409d41f
Phil Crockett <contact@philcrockett.com>
2026-08-07 06:49:58 +0200

    mise configuration

---
+++ targets/mise/configured.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env blarg
+
+REPO_PATH="${BLARG_MODULE_DIR}/config/mise"
+SYSTEM_PATH=~/.config/mise
+
+satisfied_if() {
+  checkpoint_is_current
+}
+
+apply() {
+  rm -rf "${SYSTEM_PATH}.new"
+  cp -r "${REPO_PATH}" "${SYSTEM_PATH}.new"
+  if [ -d "${SYSTEM_PATH}" ]; then
+    rm -rf "${SYSTEM_PATH}.old"
+    mv "${SYSTEM_PATH}" "${SYSTEM_PATH}.old"
+  fi
+  mv "${SYSTEM_PATH}.new" "${SYSTEM_PATH}"
+  checkpoint_success
+}
