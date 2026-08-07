2efd4ad3d112216aae57b172b6c1dc8c49711fd4
Phil Crockett <contact@philcrockett.com>
2026-08-07 07:05:33 +0200

    revert test

--- lib.d/00_checks.sh
+++ lib.d/00_checks.sh
@@ -1,6 +1,5 @@
 # shellcheck shell=bash
 
-# FIXME: just for testing
-# if [ "$(id --user)" -eq 0 ]; then
-#   panic "Don't run this as root."
-# fi
+if [ "$(id --user)" -eq 0 ]; then
+  panic "Don't run this as root."
+fi
