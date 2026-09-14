4d5278ba3b0121006ee25bd9830437bee2753d4d
Phil Crockett <contact@philcrockett.com>
2026-09-14 12:04:29 +0200

    fix(helix): replace symlink with copy

--- targets/helix/helix-configured.bash
+++ targets/helix/helix-configured.bash
@@ -4,10 +4,11 @@ LOCAL_CONFIG="${BLARG_MODULE_DIR}/config/helix"
 HELIX_CONFIG="${XDG_CONFIG_HOME}/helix"
 
 satisfied_if() {
-  test_symlink "${LOCAL_CONFIG}" "${HELIX_CONFIG}"
+  files_are_same "${LOCAL_CONFIG}/config.toml" "${HELIX_CONFIG}/config.toml"
 }
 
 apply() {
   rm -rf "${HELIX_CONFIG}"
-  ln --symbolic "${LOCAL_CONFIG}" "${HELIX_CONFIG}"
+  mkdir -p "${XDG_CONFIG_HOME}"
+  cp -r "${LOCAL_CONFIG}" "${HELIX_CONFIG}"
 }
